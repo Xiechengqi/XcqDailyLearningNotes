8a43d20e02e088fb2355f58589056bdb4e05cb98
Xiechengqi <Xiechengqiemail@163.com>
2019-03-04 23:17:17 +0800

    Create 识别系统.sh

---
+++ Linux/shell学习/识别系统.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+check_sys(){
+         if [[ -f /etc/redhat-release ]]; then
+                release='centos'
+         elif cat /etc/issue | grep -q -E -i 'debian'; then
+                release='debian'
+         elif cat /etc/issue | grep -q -E -i 'ubuntu'; then
+                release='ubuntu'
+         elif cat /etc/issue | grep -q -E -i 'centos|red hat|redhat'; then
+                release='centos'
+         elif cat /proc/version | grep -q -E -i 'debian'; then
+                release='debian'
+         elif cat /proc/version | grep -q -E -i 'centos|red hat|redhat'; then
+                release='centos'
+         elif cat /proc/version | grep -q -E -i 'ubuntu'; then
+                release='ubuntu'
+         fi
+         echo $release
+}
+check_sys
